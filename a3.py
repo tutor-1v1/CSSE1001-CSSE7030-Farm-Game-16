c519cd1b60e4330faddd58542ff1b99fcced58e4
tutor-1v1 <liumeng64@gmail.com>
2023-05-17 18:39:07 +0800

    WeChat cstutorcs CSSE1001 CSSE7030 Farm Game

---
+++ a3.py
@@ -0,0 +1,21 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+import tkinter as tk
+from tkinter import filedialog # For masters task
+from typing import Callable, Union, Optional
+from a3_support import *
+from model import *
+from constants import *
+
+# Implement your classes here
+
+def play_game(root: tk.Tk, map_file: str) -> None:
+    pass # Implement your play_game function here
+
+def main() -> None:
+    pass # Implement your main function here
+
+if __name__ == '__main__':
+    main()
